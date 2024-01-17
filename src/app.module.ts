d12efc0db4b94d818dc26c018223fbdf6d1cf447
olvneto <olvneto@yahoo.com.br>
2024-01-16 22:38:36 -0300

    Inclusão do módulo de email

--- src/app.module.ts
+++ src/app.module.ts
@@ -6,12 +6,34 @@ import { AuthModule } from './auth/auth.module';
 import { ThrottlerGuard, ThrottlerModule } from '@nestjs/throttler';
 import { APP_GUARD } from '@nestjs/core';
 import { ConfigModule } from '@nestjs/config';
+import { MailerModule } from '@nestjs-modules/mailer';
+import { PugAdapter } from '@nestjs-modules/mailer/dist/adapters/pug.adapter';
 
 @Module({
   imports: [
     ConfigModule.forRoot(),
     forwardRef(() => UserModule),
     forwardRef(() => AuthModule),
+    MailerModule.forRoot({
+      transport: {
+        host: 'smtp.ethereal.email',
+        port: 587,
+        auth: {
+          user: 'heath77@ethereal.email',
+          pass: '5QmPCs22GtM8KV4DwA',
+        },
+      },
+      defaults: {
+        from: '"Olv Soluções" <heath77@ethereal.email>',
+      },
+      template: {
+        dir: __dirname + '/templates',
+        adapter: new PugAdapter(),
+        options: {
+          strict: true,
+        },
+      },
+    }),
     ThrottlerModule.forRoot([
       {
         ttl: 60,
--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -9,6 +9,7 @@ import { PrismaService } from 'src/prisma/prisma.service';
 import { AuthRegisterDTO } from './dto/auth-register.dto';
 import { UserService } from 'src/user/user.service';
 import * as bcrypt from 'bcrypt';
+import { MailerService } from '@nestjs-modules/mailer';
 
 @Injectable()
 export class AuthService {
@@ -19,6 +20,7 @@ export class AuthService {
     private readonly jwtService: JwtService,
     private readonly prismaService: PrismaService,
     private readonly userService: UserService,
+    private readonly mailer: MailerService,
   ) {}
 
   createToken(user: User) {
@@ -89,26 +91,57 @@ export class AuthService {
       throw new UnauthorizedException('E-mail está incorreto.');
     }
 
-    //TO DO: Enviar o e-mail...
+    const token = this.jwtService.sign(
+      {
+        id: (await user).id,
+      },
+      {
+        expiresIn: '30 minutes',
+        subject: String((await user).id),
+        issuer: 'forget',
+        audience: 'users',
+      },
+    );
+
+    await this.mailer.sendMail({
+      subject: 'Recuperação de senha.',
+      to: 'chicao@email.com',
+      template: 'forget',
+      context: {
+        name: (await user).name,
+        token: token,
+      },
+    });
 
     return true;
   }
 
   async reset(password: string, token: string) {
-    //TO DO: Validar o token...
-    token;
-    const id = 0;
+    try {
+      const data: any = this.jwtService.verify(token, {
+        issuer: 'forget',
+        audience: 'users',
+      });
 
-    const user = await this.prismaService.user.update({
-      where: {
-        id,
-      },
-      data: {
-        password,
-      },
-    });
+      if (isNaN(Number(data.id))) {
+        throw new BadRequestException('O token é inválido.');
+      }
 
-    return this.createToken(user);
+      const salt = await bcrypt.genSalt();
+      password = await bcrypt.hash(password, salt);
+
+      const user = await this.prismaService.user.update({
+        where: {
+          id: Number(data.id),
+        },
+        data: {
+          password,
+        },
+      });
+      return this.createToken(user);
+    } catch (e) {
+      throw new BadRequestException(e);
+    }
   }
 
   async register(data: AuthRegisterDTO) {
